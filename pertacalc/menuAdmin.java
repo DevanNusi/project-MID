d69c0e20bcdc511e96b05d38ba3119545c0a0b55
Devan <2381012@unai.edu>
2024-10-11 22:10:18 +0600

    setup class & package

--- pertacalc/menuAdmin.java
+++ pertacalc/menuAdmin.java
@@ -1,4 +1,7 @@
 package pertacalc;
 
 public class menuAdmin {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
--- pertacalc/menuPelanggan.java
+++ pertacalc/menuPelanggan.java
@@ -1,4 +1,7 @@
 package pertacalc;
 
 public class menuPelanggan {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
